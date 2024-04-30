49d53c5343358862d216d8d799ec7c1ae9850e02
himam29 <120172716+himam29@users.noreply.github.com>
2024-04-29 22:22:43 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5"
     }
   }
+  backend "s3" {
+    bucket = "test21032024"
+    dynamodb_table = "terraform-lock"
+    region = "us-east-2"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
